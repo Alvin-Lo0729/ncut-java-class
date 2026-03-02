f200b2c9f6106755d78ee6cd00c79cdd30de5661
AlvinLo <ak47237496@gmail.com>
2026-03-02 18:21:57 +0800

    refactor Practice1on2: clean up formatting and unused code

--- src/main/java/Practice1on2.java
+++ src/main/java/Practice1on2.java
@@ -5,20 +5,25 @@ public class Practice1on2 {
 
 
   public static void main(String[] args) {
-    System.out.println("when celsius is 15, fahrenheit is " + calculateTemperature(15.0) );
+    double value1 = (double) 9 / 5;
+    double value2 = 32;
+    System.out.println("when celsius is 15, fahrenheit is " + calculateTemperature(15.0));
 
-    System.out.println("when celsius is 25, fahrenheit is " + calculateTemperature(25.0) );
+    System.out.println("when celsius is 25, fahrenheit is " + calculateTemperature(25.0));
 
-    System.out.println("when celsius is 35, fahrenheit is " + calculateTemperature(35.0) );
+    System.out.println("when celsius is 35, fahrenheit is " + calculateTemperature(35.0));
 
   }
 
+
   /**
    * 從攝氏溫度計算出華氏溫度
+   *
    * @param celsius 攝氏溫度
    * @return 華氏溫度
    */
   public static double calculateTemperature(double celsius) {
+//    return ((double) 9 /5)*celsius+32;
     return new BigDecimal("9.0").divide(new BigDecimal("5.0"), RoundingMode.HALF_UP)
         .multiply(new BigDecimal(celsius)).add(new BigDecimal(32)).doubleValue();
 
