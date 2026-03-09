b4f4c2bd717ad5d6178f6b0ce2bdeee93f5e58f7
AlvinLo <ak47237496@gmail.com>
2026-03-09 19:39:45 +0800

    fix Practice1on4: add bitwise operation examples in main method

---
+++ src/main/java/c1/Practice1on4.java
@@ -0,0 +1,19 @@
+package c1;
+
+public class Practice1on4 {
+
+  public static void main(String[] args) {
+    int a=29;
+    int b=13;
+
+    System.out.println("a & b:"+(a&b));
+    System.out.println("a | b:"+(a|b));
+    System.out.println("a ^ b:"+(a^b));
+    System.out.println("~a:"+(~a));
+
+    System.out.println("a << 2 :"+(a<<2));
+    System.out.println("a >> 2 :"+(a>>2));
+    System.out.println("a >>> 2 :"+(a>>>2));
+  }
+
+}
