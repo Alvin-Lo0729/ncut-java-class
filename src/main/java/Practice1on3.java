07a6aa6034b5becdbc6ea17a555103fd07f5dac4
AlvinLo <ak47237496@gmail.com>
2026-03-10 19:38:41 +0800

    refactor Practice1on3: remove unnecessary blank line in main method

--- src/main/java/Practice1on3.java
+++ src/main/java/Practice1on3.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 public class Practice1on3 {
 
   public static void main(String[] args) {
-
     P3Student student1 = new P3Student("1", 85, 72, false);
     P3Student student2 = new P3Student("2", 75, 60, false);
     P3Student student3 = new P3Student("3", 80, 90, true);
