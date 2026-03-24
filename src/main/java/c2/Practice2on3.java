22ef89438826814e6b5e4523c9ef92fe9e7b7abc
AlvinLo <ak47237496@gmail.com>
2026-03-24 18:27:40 +0800

    refactor Practice2on3: remove unused variables and streamline sum calculation

--- src/main/java/c2/Practice2on3.java
+++ src/main/java/c2/Practice2on3.java
@@ -4,17 +4,13 @@ public class Practice2on3 {
 
   public static void main(String[] args) {
 
-    int notebookPrice = 13;
-    int pen = 5;
-    int easer = 3;
     int count = 0;
     for (int i = 1; sum(i, 0, 0) <= 121; i++) {
       for (int j = 1; sum(i, j, 0) <= 121; j++) {
         for (int k = 1; sum(i, j, k) <= 121; k++) {
-          if (sum(i, j, k) == 121) {
-            System.out.println(
-                "筆記本" + i + "個，筆" + j + "個，橡皮擦" + k + "個=" + (notebookPrice * i + pen * j
-                    + easer * k));
+          int sum = sum(i, j, k);
+          if (sum == 121) {
+            System.out.println("筆記本" + i + "個，筆" + j + "個，橡皮擦" + k + "個=" + sum);
             count = count + 1;
           }
         }
