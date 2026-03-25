4dfbb2a10f28285813ecd0eb72b2e3c12e4ee48d
AlvinLo <ak47237496@gmail.com>
2026-03-25 10:16:16 +0800

    refactor Practice2dash4: optimize diamond pattern generation using `String.repeat` and add inverted diamond pattern

--- src/main/java/c2/Practice2dash4.java
+++ src/main/java/c2/Practice2dash4.java
@@ -1,6 +1,7 @@
 package c2;
 
 import java.util.Scanner;
+import org.apache.commons.collections4.bloomfilter.SparseBloomFilter;
 
 public class Practice2dash4 {
 
@@ -10,18 +11,15 @@ public class Practice2dash4 {
     int n = sc.nextInt();
     int total = n * 2 - 1;
     for (int i = 0; i < total; i++) {
-      int start =  i>=n?(total-i)*2-1:i*2+1;
-      int space=(total-start)/2;
-      for (int j = 0; j < space; j++) {
-        System.out.print(" ");
-      }
-      for (int k = 0; k < start; k++) {
-        System.out.print("*");
-      }
-      for (int j = 0; j < space; j++) {
-        System.out.print(" ");
-      }
-      System.out.println();
+      int start = i >= n ? (total - i) * 2 - 1 : i * 2 + 1;
+      int space = (total - start) / 2;
+      System.out.println(" ".repeat(space) + "*".repeat(start) + " ".repeat(space));
+    }
+    System.out.println(" ");
+    for (int i = 0; i < total; i++) {
+      int start = i >= n ? ((i * 2) - total) + 2 : total - (i * 2);
+      int space = (total - start) / 2;
+      System.out.println(" ".repeat(space) + "*".repeat(start) + " ".repeat(space));
     }
 
   }
