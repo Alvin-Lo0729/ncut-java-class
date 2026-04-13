c13653ca809a163ea216b8caa445ffbc39120f4d
AlvinLo <ak47237496@gmail.com>
2026-04-13 19:13:18 +0800

    fix Practice3Dash4.java: 作業3-4

---
+++ src/main/java/c3/Practice3Dash4.java
@@ -0,0 +1,25 @@
+package c3;
+
+public class Practice3Dash4 {
+
+  public static void main(String[] args) {
+    int[] array = generalIntArray();
+    int count = 0;
+    for (int i = 5; i < 18; i++) {
+      int value = array[i - 5];
+      System.out.println("i:" + i + " value: " + value);
+      count += value;
+    }
+    System.out.println(count);
+  }
+
+  private static int[] generalIntArray() {
+    int[] array = new int[13];
+    for (int i = 0; i < 300; i++) {
+      int a = (int) (Math.random() * 13) + 5;
+      array[a - 5]++;
+    }
+    return array;
+  }
+
+}
