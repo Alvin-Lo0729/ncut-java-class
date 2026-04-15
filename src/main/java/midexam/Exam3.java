da4062e14cda12c9a964fac3ed562e7c9c546bba
AlvinLo <ak47237496@gmail.com>
2026-04-15 18:57:38 +0800

    feat(期中考): 期中考3
    Exam3.java

--- src/main/java/midexam/Exam3.java
+++ src/main/java/midexam/Exam3.java
@@ -3,16 +3,31 @@ package midexam;
 public class Exam3 {
 
   private static final int MAX_NUMBER = 7;
-  private static final int LEVEL = 4;
+  private static final int LEVEL = 5;
 
   public static void main(String[] args) {
-
+    minToMore();
+    System.out.println("---------------");
+    moreToMinToMore();
   }
 
-  private static void MoreToMinToMore() {
+  private static void minToMore() {
     int total = (LEVEL * 2) - 1;
     for (int i = 0; i < total; i++) {
-      int start = (i + 1) > 4 ? 5 : total - (i * 2);
+      int start = i >= LEVEL ? total - i : i + 1;
+      int space = LEVEL - start;
+      System.out.println(
+          " ".repeat(space) + String.valueOf(start).repeat(start));
+    }
+  }
+
+  private static void moreToMinToMore() {
+    int total = (LEVEL * 2) - 1;
+    for (int i = 0; i < total; i++) {
+      int start = i >= LEVEL ? ((i * 2) - total) + 2 : total - (i * 2);
+      int space = (total - start) / 2;
+      System.out.println(
+          " ".repeat(space) + String.valueOf(start).repeat(start) + " ".repeat(space));
     }
   }
 
