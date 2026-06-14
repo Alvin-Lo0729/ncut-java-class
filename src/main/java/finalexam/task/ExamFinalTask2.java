2f5adb85c69021d9db666d131f1a3e3fb94f0e7a
AlvinLo <ak47237496@gmail.com>
2026-06-14 23:05:14 +0800

    feat(6/14): 期末考習題

--- src/main/java/finalexam/task/ExamFinalTask2.java
+++ src/main/java/finalexam/task/ExamFinalTask2.java
@@ -8,6 +8,7 @@ public class ExamFinalTask2 implements Runnable {
     for (int i = 0; i < 6; i++) {
       System.out.println("task2 word: " + "B" + " Counter:" + (i + 1));
       try {
+        //這裡做等待動作
         Thread.sleep(800);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
