b971241c599640f3693b1cb09801076d052ceeef
AlvinLo <ak47237496@gmail.com>
2026-06-16 21:43:45 +0800

    feat(6/14): 期末考習題

--- src/main/java/finalexam/FinalExam2.java
+++ src/main/java/finalexam/FinalExam2.java
@@ -1,8 +1,6 @@
 package finalexam;
 
-import finalexam.task.ExamFinalTask1;
-import finalexam.task.ExamFinalTask2;
-import finalexam.task.ExamFinalTask3;
+import finalexam.task.ExamFinalTask;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,11 +10,14 @@ public class FinalExam2 {
   public static void main(String[] args) {
 
     try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
-      CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(new ExamFinalTask1(),
+      CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(
+          new ExamFinalTask(600L, "A", 8),
           executor).thenAccept(unused -> System.out.println("Task1 thread Finally"));
-      CompletableFuture<Void> completableFuture2 = CompletableFuture.runAsync(new ExamFinalTask2(),
+      CompletableFuture<Void> completableFuture2 = CompletableFuture.runAsync(
+          new ExamFinalTask(800L, "B", 6),
           executor).thenAccept(unused -> System.out.println("Task2 thread Finally"));
-      CompletableFuture<Void> completableFuture3 = CompletableFuture.runAsync(new ExamFinalTask3(),
+      CompletableFuture<Void> completableFuture3 = CompletableFuture.runAsync(
+          new ExamFinalTask(1000L, "C", 5),
           executor).thenAccept(unused -> System.out.println("Task3 thread Finally"));
 
       CompletableFuture.allOf(completableFuture, completableFuture2, completableFuture3).join();
---
+++ src/main/java/finalexam/task/ExamFinalTask.java
@@ -0,0 +1,27 @@
+package finalexam.task;
+
+public class ExamFinalTask implements Runnable {
+
+  private final long millis;
+  private final String word;
+  private final int times;
+
+  public ExamFinalTask(long millis, String word, int times) {
+    this.millis = millis;
+    this.word = word;
+    this.times = times;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 0; i < times; i++) {
+      System.out.println("word: " + word + " Counter:" + (i + 1));
+      try {
+        //這裡做等待動作
+        Thread.sleep(millis);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+}
